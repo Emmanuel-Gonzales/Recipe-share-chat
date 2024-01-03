2301dd2e87d3be2fb784f555b79b97c011575062
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2024-01-03 00:57:12 -0600

    add server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Welcome to the Backend');
+});
+
+function start(port){
+  app.listen(port, () => console.log(`listening on port ${port}`));
+}
+
+module.exports = {start, app};
